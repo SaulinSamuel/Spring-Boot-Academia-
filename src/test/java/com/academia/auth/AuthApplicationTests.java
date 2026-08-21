b2f1f5921c4ba76e9232ede4f64f1bcf6290f867
Saulin Samuel <saulosamuellealtorres4@gmail.com>
2026-08-21 00:11:50 -0300

    fix: TestContainersConfig and pom.xml hikari tentando validar conexões já fechadas

--- src/test/java/com/academia/auth/AuthApplicationTests.java
+++ src/test/java/com/academia/auth/AuthApplicationTests.java
@@ -2,10 +2,14 @@ package com.academia.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+
+import com.academia.auth.config.TestContainersConfig;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestContainersConfig.class)
 class AuthApplicationTests {
 
 	@Test
--- src/test/java/com/academia/auth/config/TestContainersConfig.java
+++ src/test/java/com/academia/auth/config/TestContainersConfig.java
@@ -3,8 +3,19 @@ package com.academia.auth.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 
+import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
+
 @TestConfiguration(proxyBeanMethods = false)
 @ImportTestcontainers(TestContainers.class)
 public class TestContainersConfig {
     
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                AbandonedConnectionCleanupThread.checkedShutdown();
+            } catch (Exception ignorar) {
+            
+            }
+        }, "mysql-cleanup-shutdown-hook"));
+    }
 }

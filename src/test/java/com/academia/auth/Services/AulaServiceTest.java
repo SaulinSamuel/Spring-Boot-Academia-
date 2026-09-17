48ebfb369aac74e87681c429e6ff7932f4e0438b
Saulin Samuel <saulosamuellealtorres4@gmail.com>
2026-09-16 23:16:38 -0300

    test: fix test mock event publish in AulaServiceTest

--- src/test/java/com/academia/auth/Services/AulaServiceTest.java
+++ src/test/java/com/academia/auth/Services/AulaServiceTest.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -45,6 +46,9 @@ public class AulaServiceTest {
 
     @Mock
     private UsuarioAutenticadoService usuarioLogado;
+
+    @Mock
+    private ApplicationEventPublisher applicationEventPublisher;
 
     @InjectMocks
     private AulaService aulaService;
